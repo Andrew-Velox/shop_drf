d59c21c867b9077e35d2cbb51292e18cb08b21f0
andrew-velox <mohabbat.bd2020@gmail.com>
2025-07-23 12:56:48 +0600

    add login signup url of front end

--- customers/views.py
+++ customers/views.py
@@ -38,7 +38,7 @@ class UserRegistrationApiViewset(APIView):
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
 
-            confirm_link = f"http://127.0.0.1:8000/users/account/active/{uid}/{token}"
+            confirm_link = f"https://shopdrf-production.up.railway.app/users/account/active/{uid}/{token}"
 
             email_subject = "confirm Your Email"
             email_body = render_to_string('confirm_account_email.html',{'confirm_link':confirm_link})
@@ -59,8 +59,8 @@ def activate(request,uid64,token):
     if user is not None and default_token_generator.check_token(user,token):
         user.is_active=True
         user.save()
-        return redirect("login")
-    return redirect("register")
+        return redirect("https://mew-shop-eight.vercel.app/login")
+    return redirect("https://mew-shop-eight.vercel.app/register")
 
 
 class UserLoginApiView(APIView):
